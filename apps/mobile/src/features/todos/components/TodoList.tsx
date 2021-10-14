88e730e9c1945efdb0bc675762b4232dadb10660
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-14 16:25:14 +0200

    mobile: add bottom tab navigator

--- apps/mobile/src/features/todos/components/TodoList.tsx
+++ apps/mobile/src/features/todos/components/TodoList.tsx
@@ -1,61 +1,29 @@
 import { Todo } from '@smartlists/shared-types';
 import React, { useCallback } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, Text } from 'react-native';
+import { useTodos } from '../hooks/useTodos';
 import styled from 'styled-components/native';
 
-import { useTodos } from '../hooks/useTodos';
-import { Check } from '../../../assets/icons/CheckIcon';
-import { UnChecked } from '../../../assets/icons/UnCheckedIcon';
-import { Close } from '../../../assets/icons/CloseIcon';
-import { Colors } from '../../../libs/ui/theme';
-
 export const TodoList = () => {
-  const { todos, toggleTodo, deleteTodo } = useTodos();
+  const { todos, toggleTodo } = useTodos();
 
   const renderItem = useCallback(
     ({ item: todo }: { item: Todo }) => (
-      <Row>
-        <ToggleTouchableOpacity
-          flex={8}
-          onPress={() => toggleTodo(todo.id, todo.done)}
-        >
-          {todo.done ? (
-            <Check size={24} color={Colors.primary} />
-          ) : (
-            <UnChecked size={24} color={Colors.foreground} />
-          )}
-          <Text>{todo.text}</Text>
-        </ToggleTouchableOpacity>
-        <DeleteTouchableOpacity onPress={() => deleteTodo(todo.id)}>
-          <Close size={24} color={Colors.secondary} />
-        </DeleteTouchableOpacity>
-      </Row>
+      <TouchableOpacity onPress={() => toggleTodo(todo.id, todo.done)}>
+        <Text>{todo.done ? '✅' : '⭕️'}</Text>
+        <Text> </Text>
+        <Text>{todo.text}</Text>
+      </TouchableOpacity>
     ),
-    [toggleTodo, deleteTodo]
+    [toggleTodo]
   );
 
   return <FlatList data={todos} renderItem={renderItem} />;
 };
 
-const Row = styled.View({
+const TouchableOpacity = styled.TouchableOpacity({
   flexDirection: 'row',
-  justifyContent: 'space-between',
+  alignItems: 'center',
   marginHorizontal: 24,
   paddingVertical: 12,
 });
-
-const ToggleTouchableOpacity = styled.TouchableOpacity({
-  flex: 8,
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-});
-
-const DeleteTouchableOpacity = styled.TouchableOpacity({
-  flex: 1,
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'center',
-});
-
-const Text = styled.Text({ paddingLeft: 12 });
