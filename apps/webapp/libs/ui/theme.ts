e2a950c76770fb24d78c1f70faf436dd2e88e175
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-10-14 21:54:12 +0200

    webapp: add navigation topbar

---
+++ apps/webapp/libs/ui/theme.ts
@@ -0,0 +1,7 @@
+// Taken from https://colorhunt.co/palette/30384100adb5eeeeeeff5722
+export enum Colors {
+  primary = '#00ADB5',
+  secondary = '#FF5722',
+  background = '#EEEEEE',
+  foreground = '#303841',
+}
